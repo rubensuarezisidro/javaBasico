6fdab5a3d86d9ce3e3a2a2a8a10ed1c87be10375
alumno <alumno@LAB706-11>
2013-11-02 16:25:20 -0500

    proyecto base

--- src/javabasico/JavaBasico.java
+++ src/javabasico/JavaBasico.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javabasico;
 
-/**
- *
- * @author alumno
- */
 public class JavaBasico {
 
    

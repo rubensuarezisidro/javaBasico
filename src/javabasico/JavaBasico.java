2711d199b75221ce778dbdfd1c156cd2f159d5ab
alumno <alumno@LAB706-11>
2013-11-02 16:21:37 -0500

    proyecto base

--- src/javabasico/JavaBasico.java
+++ src/javabasico/JavaBasico.java
@@ -10,10 +10,8 @@ package javabasico;
  */
 public class JavaBasico {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola ");
     }
 }
